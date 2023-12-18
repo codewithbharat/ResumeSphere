edc8bcb9088c709340b744decc6f612a059bd7bc
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-12-19 01:46:19 +0530

    login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+    const inputFileds = [
+        {
+            name: 'Email',
+            type: 'email'
+        },
+        {
+            name: 'Password',
+            type: 'password'
+        }
+    ]
     return (
         <Layout>
-            Login
+            <div className="flex justify-center items-center">
+                <div className=" md:basis-4/6 lg:basis-2/3 xl:basis-1/2 basis-5/6 p-2 flex items-center justify-center ">
+                    <div className="w-full bg-blue-200/60 shadow-md rounded-md p-4 md:pl-12 md:py-4">
+                        <h1 className='text-2xl md:text-4xl font-semibold'>Get Started Now</h1>
+                        <form className='flex flex-col  md:w-10/12 py-2'>
+                            {
+                                inputFileds.map((input) => (
+                                    <div className="flex flex-col my-2">
+                                        <label htmlFor={input.name} className='text-xl mb-2'>{input.name}</label>
+                                        <input required type={input.type} name={input.name} id={input.name} className='py-2 px-4 rounded-md border-2 border-gray-400 focus:border-blue-400 focus:outline-none' />
+                                    </div>
+                                ))
+                            }
+                            <div>
+
+                                <input type="submit" className="bg-blue-400 text-white p-2 rounded-md shadow-sm" value="Login" />
+                            </div>
+                        </form>
+
+                        <div className='text-center'>
+                            <p className='md:text-xl'> Don't have an account? <Link to="/register" className='underline text-blue-700'>register</Link></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
