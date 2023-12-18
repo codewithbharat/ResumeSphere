ee1096ea50d940911bba0702617c9443f1e69d6d
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-12-18 17:33:25 +0530

    layoutBug Fixed

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Navbar from './Navbar'
+import './styles/Layout.css'
 
 const Layout = ({ children }) => {
     return (
-        <div className="flex flex-col w-full h-screen overflow-hidden">
-            <div className="h-1/12 py-2">
+        <div className=" min-h-screen flex flex-col">
+            <header className=" p-4">
                 <Navbar />
-            </div>
-            <div className="h-11/12 h-full">
-                <div className='bg-slate-300 h-full px-12 text-white overflow-auto '>
-                    {children}
+            </header>
+
+            <main className="container mx-auto my-4 flex-grow">
+                {/* Body content goes here */}
+                {children}
+            </main>
+
+            <footer className="bg-gray-300 p-4">
+                {/* Footer content goes here */}
+                <div className="flex justify-center">
+                    <p className="text-sm text-gray-600">© 2023 codewithbharat All rights reserved. | Source Code : <a className='font-semibold' href="https://github.com/codewithbharat/ResumeSphere">Github</a> </p>
                 </div>
-            </div>
-        </div>
+            </footer>
+        </div >
     )
 }
 
