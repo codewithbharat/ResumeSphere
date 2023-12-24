5d53dacf354bbbdcce247707f9d12fae683dc2f7
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-12-24 12:49:12 +0530

    skill Frontend done

--- client/src/pages/MoreInfo/Skill.jsx
+++ client/src/pages/MoreInfo/Skill.jsx
@@ -1,19 +1,101 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DasboardLayout from '../../components/DashboardLayout'
+import axios from 'axios'
 
 const Skill = () => {
+
+    const user = JSON.parse(localStorage.getItem('user'))
+    const token = localStorage.getItem('token')
+
+    const [skillsData, setSkillsData] = useState([]);
 
     const [skill, setSkill] = useState({
         skill: '',
         proficiency: ''
     })
+
+    const getUserData = () => {
+        axios.get(`${import.meta.env.VITE_SERVER}/${user._id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(res => {
+                const { user } = res.data;
+                localStorage.setItem('user', JSON.stringify(user));
+                setSkillsData(user.skill);
+                console.log(user.skill);
+            }
+            )
+
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+
+    useEffect(() => {
+        getUserData();
+    }, [])
+
+    const handleChange = (e) => {
+        setSkill({ ...skill, [e.target.name]: e.target.value });
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post(`${import.meta.env.VITE_SERVER}/${user._id}/skill`, skill, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            const { message } = res.data;
+            console.log(message);
+
+            // get user data again
+            getUserData();
+
+            // clear form
+            setSkill({
+                skill: '',
+                proficiency: ''
+            })
+        }).catch((err) => {
+            console.log(err);
+        })
+
+
+    }
+
+    const deleteSkill = (skill_id) => {
+        axios.delete(`${import.meta.env.VITE_SERVER}/${user._id}/skill/${skill_id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(res => {
+                const { message } = res.data;
+                console.log(message);
+
+                // get user data again
+                getUserData();
+
+            }
+            )
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     return (
         <DasboardLayout>
             <div className="flex flex-col px-2 md:px-4 lg:px-12">
                 <h1 className='text-4xl'>Skills</h1>
                 <div className="flex flex-col-reverse md:flex-col">
                     <form
-
+                        onSubmit={handleSubmit}
                         className='bg-white p-2 md:p-4 rounded-md shadow-md my-4 '
                     >
                         <div className="flex flex-col md:flex-row">
@@ -23,7 +105,7 @@ const Skill = () => {
                                         <label htmlFor="skill" className='text-2xl font-semibold' >Skill</label>
                                         <input type="text" name="skill" id="skill" value={skill.skill}
                                             className='border-2 border-indigo-300 rounded-md p-2 my-2 text-xl'
-                                            placeholder='ReactJS'
+                                            placeholder='ReactJS' onChange={handleChange}
                                             required
                                         />
                                     </div>
@@ -34,7 +116,7 @@ const Skill = () => {
                                             id="proficiency"
                                             value={skill.proficiency}
                                             className='border-2 border-indigo-300 rounded-md p-2 my-2 text-xl'
-                                            required
+                                            required onChange={handleChange}
                                         >
                                             <option value="" disabled>Select...</option>
                                             <option value="beginner">Beginner</option>
@@ -62,10 +144,23 @@ const Skill = () => {
                         </div>
                     </form>
 
-                    <div className="flex flex-col">
+                    <div className="flex flex-col md:flex-row md:flex-wrap">
+                        {skillsData && skillsData.map((skill, index) => (
+                            <div key={index} className='bg-white p-2 md:p-4 rounded-md shadow-md m-4'>
+                                <div className="flex flex-col">
+                                    <p className='text-2xl font-bold'>{skill.skill} <span className='font-normal'>- {skill.proficiency}</span></p>
+                                    {/* Add more details here if needed */}
+                                </div>
 
-                        {/* skillsData */}
-
+                                {/* Delete button */}
+                                <button
+                                    onClick={() => deleteSkill(skill._id)} // Replace with your actual delete function
+                                    className='bg-red-500 cursor-pointer text-white rounded-md px-4 py-2 mt-6 text-xl'
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        ))}
                     </div>
                 </div>
 
